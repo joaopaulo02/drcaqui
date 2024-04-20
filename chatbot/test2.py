698062f7700725d5cc4a028baa14a8576282dc50
joaopaulo02 <jpalonso0209@gmail.com>
2024-04-20 19:16:36 -0300

    env variables updated.

--- chatbot/test2.py
+++ chatbot/test2.py
@@ -3,12 +3,12 @@ import boto3
 import json
 
 brt = boto3.client(
-    service_name='bedrock-runtime',
-    region_name='us-east-1',
-    aws_access_key_id='ASIA4MTWHZPHBRQ4GQCB',
-    aws_secret_access_key='INuPCUsYPtTTZ+OezROL5JFzMNNoaJV3csUVNVft',
-    aws_session_token='IQoJb3JpZ2luX2VjEOr//////////wEaCXVzLWVhc3QtMSJHMEUCIQDYBACE395SebzaO31EJqTKdfA4dns06oGGq0o6/jUlSQIgMRSg31QlHqOEhwne8L4hirSgx5DiR2Qpygd1/eUXD5oqiwMIMxAAGgw4NTE3MjUyNDEyOTQiDIDI2bt1SDwTAGLyoyroAnGrk9Iooa/RTWaTpPtT1H6EJwa5CT8HMgMud0zYKwFazya77lNRmD/9KJe43ugbpYQUmpfE9Ird1qBBG0NzgxXptad8BjCl/weLLZZfrXQwhKT8SdaY+RBXRrm4UHq/j6WQfMdfyPKoa/pz20strq9EH+wYpXkKDTAbjGfUqWz6beGlND647qjZxB4THum6GHQNgxNMgKRyuELEFGWAAex2cjIhFkij8ZFAXPD8E+6qr0TiIZ+VnPh9hKPFIpHcHUJQq7smWIvY0ws+e+dtk2Bd37WDcq4tZ3q7TqjJ5p1IBb4sod/goKH0dn60Hlq/QlGI3zmd+QcDfe5PrTCwb3lOlfFxvr9UpSZY/KBqHmtgvUostJcSr7XK8deOZ7D2sQ8TO8dcFBqKFMnbUM7r9oMMMSo5BZfLCXRLoYf83CDDZgMb7ZzYdDxq9uPZMvkPm9OclG41LMzMh5MT8QjUheAiDJZL6oBtqzC984+xBjqmAaN0C2uPH7XnDncOccZIQdp244fkllRJLFaYIEHJfNvhubjo0AZZeddTJ6cVa+TOPVt3jZwqnbGApjmS6i2Yx5PyIRY/tNmUPZ41NuIXYwDaZSTNdf6mLfe6wWg8DNSJ4v40Wp2kraC267bW9Sl7CMvDsBzLW/+Tbzkbyx+i3rDpLJAiYO2fC6pwKu1HEygF+Vb1gbS8Ds2z1y/UBC/bIgfV6CLXPAo=',
-    endpoint_url='https://bedrock-runtime.us-east-1.amazonaws.com'
+    service_name=os.getenv('SERVICE_NAME'),
+    region_name=os.getenv('REGION_NAME'),
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
+    endpoint_url=os.getenv('ENDPOINT_URL')
 )
 
 # Crie a instância do Flask
